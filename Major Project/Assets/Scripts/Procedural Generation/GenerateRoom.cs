7a8edba7ec81b39e772a87dbcf5b56c175c00a12
SurlawCitebaid <91384129+SurlawCitebaid@users.noreply.github.com>
2022-04-06 16:29:59 +1000

    Different Sized Levels

---
+++ Major Project/Assets/Scripts/Procedural Generation/GenerateRoom.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateRoom : MonoBehaviour
+{
+    Room room;
+    public int roomSizeX;
+    public int roomSizeY;
+    int[,] roomData;
+    //Wall prefab
+    public GameObject wall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        room = new Room(roomSizeX, roomSizeY, RoomType.CIRCUS);
+        roomData = new int[roomSizeX, roomSizeY];
+        fillRoomData();
+        generateRoom();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void fillRoomData()
+    {
+        for (int x = 0; x < roomSizeX; x++)
+        {
+            for (int y = 0; y < roomSizeY; y++)
+            {
+                //Border and a border
+                if (x == 0 || x == roomSizeX-1 || y == 0 || y == roomSizeY - 1)
+                {
+                    roomData[x, y] = 1;
+                }
+                else
+                {
+                    roomData[x, y] = 0;
+                }
+            }
+        }
+    }
+
+    void generateRoom()
+    {
+        for (int x = 0; x < roomSizeX; x++)
+        {
+            for (int y = 0; y < roomSizeY; y++)
+            {
+                //Border and a border
+                if (roomData[x, y] == 1)
+                {
+                   Instantiate(wall, new Vector3((float)x, (float)y), Quaternion.identity);
+                }
+            }
+        }
+    }
+}
--- Major Project/Assets/Scripts/Procedural Generation/Room.cs
+++ Major Project/Assets/Scripts/Procedural Generation/Room.cs
@@ -2,8 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Room : ScriptableObject
+public class Room
 {
     int roomSizeX;
     int roomSizeY;
+    RoomType roomType;
+    int[,] roomData;
+
+    public Room(int roomSizeX, int roomSizeY, RoomType roomType)
+    {
+        this.roomType = roomType;
+        this.roomSizeX = roomSizeX;
+        this.roomSizeY = roomSizeY;
+        int[,] roomData = new int[roomSizeX,roomSizeY];
+        for(int x = 0; x < roomSizeX; x++)
+        {
+            for(int y = 0; y < roomSizeY; y++)
+            {
+                roomData[x,y] = 0;
+            }
+        }
+    }
+
+    public int[,] getRoomData()
+    {
+        return roomData;
+    }
+}
+
+public enum RoomType
+{
+    CIRCUS
 }
