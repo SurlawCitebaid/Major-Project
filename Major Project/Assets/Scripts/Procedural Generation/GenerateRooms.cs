eed4275b73d321aa834b228fee680bef624f97ce
SurlawCitebaid <91384129+SurlawCitebaid@users.noreply.github.com>
2022-04-06 14:41:28 +1000

    fgfd

--- Major Project/Assets/Scripts/Procedural Generation/GenerateRooms.cs
+++ Major Project/Assets/Scripts/Procedural Generation/GenerateRooms.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestScript : MonoBehaviour
+public class GenerateRooms : MonoBehaviour
 {
-    //test test
-    // yet another test
     // Start is called before the first frame update
     void Start()
     {
