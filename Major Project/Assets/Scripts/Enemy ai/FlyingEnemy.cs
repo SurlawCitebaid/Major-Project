2aec5cbf29a1f780c9cd434c3714d99945f9f424
xXShadowSlayer64Xx <101858727+xXShadowSlayer64Xx@users.noreply.github.com>
2022-04-13 15:10:56 +1000

    Update FlyingEnemy.cs

--- Major Project/Assets/Scripts/Enemy ai/FlyingEnemy.cs
+++ Major Project/Assets/Scripts/Enemy ai/FlyingEnemy.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class FlyingEnemy : MonoBehaviour
 {
     private Rigidbody2D rigid;
-    private float thrust = 8f, speed = 3f, attackRange = 4f, flightHeight, yPos, angle;
+    private float thrust = 4f, speed = 3f, attackRange = 4f, flightHeight, yPos, angle;
     private GameObject player;
+    private enum State { MOVING, CHASE, AIMING, ATTACKING, COOLDOWN };
+    private State state;
     private bool inPosition = false, attacked = false, aiming = false;
     private Quaternion q, x;
     private Vector3 vectorToTarget, startRot;
     // Start is called before the first frame update
     void Start()
     {
+        state = State.MOVING;
         startRot = transform.position;
         x = transform.rotation;
         yPos = transform.position.y;                                    //consistent yPos
         rigid = GetComponent<Rigidbody2D>();                                //get ai physics
-        flightHeight = Random.Range(1f, 4f);
+        flightHeight = Random.Range(0f, 8f);
         player = GameObject.FindGameObjectWithTag("Player");                //so ai knows where player is
     }
 
@@ -27,30 +30,38 @@ public class FlyingEnemy : MonoBehaviour
 
         if (player == null)
             return;
-        if (yPos < player.transform.position.y && inPosition == false)              //true false false 
+
+        switch (state)
         {
-            rePosition();                                           //enemy below player
-        }
-        else if (yPos >= player.transform.position.y && inPosition == false)        //true false false 
-        {
-            inPosition = true;                                       //enemy above player
-        }
-        else
-        {
-            flightHeight = Random.Range(0f, 4f);
-            float dist = Mathf.Abs(transform.position.x - player.transform.position.x);
-            if (dist > attackRange && !attacked)
-            {
-                chase();                                                        //true true true 
-            }
-            else
-            {
-                if (aiming)
+            case State.MOVING:
+                if (yPos < player.transform.position.y)
                 {
-                    aimAttack();
+                    rePosition();
                 }
-                Invoke("attack", 2f);                                       //true false true 
-            }
+                else if (yPos >= player.transform.position.y)
+                {
+                    state = State.CHASE;
+
+                }
+                break;
+            case State.CHASE:
+                flightHeight = Random.Range(0f, 9f);
+                float dist = Mathf.Abs(transform.position.x - player.transform.position.x);
+                if (dist > attackRange)
+                {
+                    chase();
+                } else
+                {
+                    state = State.AIMING;
+                }
+                break;
+            case State.AIMING:
+
+                aimAttack();
+                break;
+            case State.ATTACKING:
+                Invoke("attack", 2f);
+                break;
         }
     }
     private void aimAttack()
@@ -59,20 +70,18 @@ public class FlyingEnemy : MonoBehaviour
         angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - 90f;
         q = Quaternion.AngleAxis(angle, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
-
+        if(transform.rotation == q)
+        {
+            state = State.ATTACKING;
+        }
     }
     private void attack()
     {
-        if (attacked == false)
-        {
-            attacked = true;
-            aiming = false;
-            rigid.AddForce(transform.up * thrust, ForceMode2D.Impulse);
-        }
+        rigid.AddForce(transform.up * thrust, ForceMode2D.Impulse);
+        
     }
     private void chase()
     {
-        aiming = true;
         transform.position = Vector2.MoveTowards(transform.position, new Vector2(player.transform.position.x, transform.position.y), speed * Time.deltaTime);
 
     }
@@ -82,32 +91,16 @@ public class FlyingEnemy : MonoBehaviour
         if (Mathf.Abs(transform.position.y) == Mathf.Abs(flightHeight + player.transform.position.y)) //you cant compare float values by themselves
         {
             yPos = transform.position.y;
-            attacked = false;
         }
     }
     private void reset()
     {
         yPos = transform.position.y;
-        inPosition = false;
-
-    }
-    IEnumerator resetPos()
-    {
-
-        float elapsedTime = 0.0f;
-        float time = 100f;
-        while (elapsedTime < time)
-        {
-            elapsedTime += Time.deltaTime;
-            transform.rotation = Quaternion.Slerp(transform.rotation, x, (elapsedTime / time));
-        }
-        yield return new WaitForSeconds(1f);
     }
     void OnTriggerEnter2D(Collider2D col)
     {
+        Debug.Log("ASS");
         rigid.velocity = Vector3.zero;
         rigid.angularVelocity = 0f;
-        StartCoroutine(resetPos());
-        Invoke("reset", 3f);
     }
 }
