4b22aca31167a3c9d5b14631151e0b4fcd626254
xXShadowSlayer64Xx <101858727+xXShadowSlayer64Xx@users.noreply.github.com>
2022-04-13 13:48:53 +1000

    Air unit

--- Major Project/Assets/Scripts/Enemy ai/FlyingEnemy.cs
+++ Major Project/Assets/Scripts/Enemy ai/FlyingEnemy.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 public class FlyingEnemy : MonoBehaviour
 {
     private Rigidbody2D rigid;
-    private float thrust = 20f;
-    public float speed;
+    private float thrust = 8f, speed = 3f, attackRange = 4f, flightHeight, yPos, angle;
     private GameObject player;
-    private bool attacked = true;
-    private float flightHeight;
-    private float attackRange = 4f;
+    private bool inPosition = false, attacked = false, aiming = false;
+    private Quaternion q, x;
+    private Vector3 vectorToTarget, startRot;
     // Start is called before the first frame update
     void Start()
     {
-        rigid = GetComponent<Rigidbody2D>();
-        flightHeight = Random.Range(1f, 4f);
-        player = GameObject.FindGameObjectWithTag("Player");
+        startRot = transform.position;
+        x = transform.rotation;
+        yPos = transform.position.y;                                    //consistent yPos
+        rigid = GetComponent<Rigidbody2D>();                                //get ai physics
+        flightHeight = Random.Range(1f, 4f);                                
+        player = GameObject.FindGameObjectWithTag("Player");                //so ai knows where player is
     }
 
     // Update is called once per frame
@@ -25,39 +27,98 @@ public class FlyingEnemy : MonoBehaviour
 
         if (player == null)
             return;
-
-        if (transform.position.y - flightHeight <= player.transform.position.y && attacked == true)
+        if (yPos < player.transform.position.y && inPosition == false)              //true false false 
         {
             rePosition();                                           //enemy below player
-        } else if (transform.position.y - flightHeight >= player.transform.position.y && attacked == true)
+        }
+        else if (yPos >= player.transform.position.y && inPosition == false)        //true false false 
         {
-            attacked = false;                                       //enemy above player
-        }else
+            inPosition = true;                                       //enemy above player
+        }
+        else
         {
-            flightHeight = Random.Range(1f, 4f);
-            float dist = Vector2.Distance(player.transform.position, transform.position);
-            if (dist > attackRange)
+            flightHeight = Random.Range(0f, 4f);
+            float dist = Mathf.Abs(transform.position.x - player.transform.position.x);
+            if (dist > attackRange && !attacked )
             {
-                chase();
+                chase();                                                        //true true true 
             }
             else
-                attack();
+            {
+                if (aiming)
+                {
+                    aimAttack();
+                } 
+                    Invoke("attack", 2f);                                       //true false true 
+            }
         }
+    }
+    private void aimAttack()
+    {
+        vectorToTarget = player.transform.position - transform.position;
+        angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - 90f;
+        q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
+
     }
     private void attack()
     {
-        rigid.AddForce(transform.right * thrust);
+        if (attacked == false)
+        {
+            attacked = true;
+            aiming = false;
+            rigid.AddForce(transform.up * thrust, ForceMode2D.Impulse);
+        }
     }
     private void chase()
     {
-        transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), new Vector2(player.transform.position.x,transform.position.y ), speed * Time.deltaTime);
+        aiming = true;
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(player.transform.position.x,transform.position.y ), speed * Time.deltaTime);
+
     }
     private void rePosition()
     {
         transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, player.transform.position.y+ flightHeight), speed * Time.deltaTime);
         if (Mathf.Abs(transform.position.y) == Mathf.Abs(flightHeight +player.transform.position.y) ) //you cant compare float values by themselves
         {
+            yPos = transform.position.y;
             attacked = false;
         }
+    }
+    private void reset()
+    {
+        yPos = transform.position.y;
+        inPosition = false;
+        
+    }
+    private bool isReset()
+    {
+        if (x == transform.rotation)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    IEnumerator resetPos()
+    {
+        float elapsedTime = 0.0f;
+        float time = 100f;
+        while(elapsedTime < time)
+        {
+            elapsedTime += Time.deltaTime;
+            transform.rotation = Quaternion.Slerp(transform.rotation, x, (elapsedTime / time));
+            
+            yield return new WaitUntil(isReset);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        rigid.velocity = Vector3.zero;
+        rigid.angularVelocity = 0f;
+        StartCoroutine(resetPos());
+        Invoke("reset", 3f);
     }
 }
