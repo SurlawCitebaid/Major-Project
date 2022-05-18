66a1efdc8d8dcb2ae869fca127e18c958d2a0fc0
xXShadowSlayer64Xx <101858727+xXShadowSlayer64Xx@users.noreply.github.com>
2022-05-18 17:51:16 +1000

    projectile bugfix

--- Major Project/Assets/Scripts/Enemy ai/Projectile.cs
+++ Major Project/Assets/Scripts/Enemy ai/Projectile.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    private Vector3 shootDir;  
+    private Vector3 shootDir;
     public void Setup(Vector3 shootDir)
     {
         this.shootDir = shootDir;
     }
     private void Update()
     {
-        float moveSpeed = 100f;
+        float moveSpeed = 80f;
         transform.Translate(shootDir * moveSpeed * Time.deltaTime);
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
     }
 }
