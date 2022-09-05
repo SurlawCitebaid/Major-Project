d8aefce19fbf44cf937d304acc324814db740473
julietgobran00 <71473941+julietgobran00@users.noreply.github.com>
2022-09-06 08:07:55 +1000

    Health sprites n script
    need to link to player

---
+++ Major Project/Assets/HealthController.cs
@@ -0,0 +1,34 @@
+// Juliet Gobran
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour
+{
+    public int playerHealth;
+    //Color pink = Color.FromArgb(255, 105, 249);
+
+    [SerializeField] private Image[] healthHearts;
+
+    private void Start()
+    {
+        UpdateHealthStatus;
+    }
+
+    private void UpdateHealthStatus()
+    {
+        for (int i = 0; i < healthHearts.length; i++)
+        {
+            if (i < playerHealth)
+            {
+                healthHearts[i].color = Color.red;
+            }
+            else
+            {
+                healthHearts[i].color = Color.black;
+            }
+        }
+    }
+}
