f0e8c8bd08f4b5b7b81cf4c076c11dbb9cd8d038
julietgobran00 <71473941+julietgobran00@users.noreply.github.com>
2022-09-23 10:08:03 +1000

    fixing volume slider

--- Major Project/Assets/Scripts/UI/SettingsMenu.cs
+++ Major Project/Assets/Scripts/UI/SettingsMenu.cs
@@ -9,43 +9,18 @@ using System.Linq;
 
 public class SettingsMenu : MonoBehaviour
 {
-  
     // Sets volume as slider moves
     public AudioMixer aM;
+
+    private void start()
+    {
+        float currentLevel;
+        aM.GetFloat("MasterVolume", out currentLevel);
+        SetVolume(currentLevel);
+    }
+
     public void SetVolume (float vol)
     {
         aM.SetFloat("MasterVolume", vol);
     }
-
-    // NOT IN USE ANYMORE
-    ToggleGroup toggleGroup;
-    void Start()
-    {
-        toggleGroup = GetComponent<ToggleGroup>();
-    }
-
-    public void SetGraphics()
-    {
-        Toggle toggle = toggleGroup.ActiveToggles().FirstOrDefault();
-        int qualityIndex = 2;
-
-        if (toggle.name == "Toggle Low")
-        {
-            qualityIndex = 0;
-        } 
-        else if (toggle.name == "Toggle Med")
-        {
-            qualityIndex = 1;
-        } 
-        else if (toggle.name == "Toggle High")
-        {
-            qualityIndex = 2;
-        }
-        QualitySettings.SetQualityLevel(qualityIndex);
-
-        Debug.Log("Updated Quality to index " + toggle.name);
-    }
-
-
-
 }
