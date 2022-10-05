7e09c357a1b634387e972557793f80f35ac5543b
julietgobran00 <71473941+julietgobran00@users.noreply.github.com>
2022-10-05 11:56:59 +1100

    Time added to gameover screen

--- Major Project/Assets/Scripts/UI/GameOverPopup.cs
+++ Major Project/Assets/Scripts/UI/GameOverPopup.cs
@@ -3,10 +3,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameOverPopup : MonoBehaviour
 {
+    public TMP_Text timeValue;
+
+    void Update() 
+    {
+        timeValue.text = Time.time.ToString("0.00");
+    }
+
     public void Restart()
     {
         Time.timeScale = 1f;
