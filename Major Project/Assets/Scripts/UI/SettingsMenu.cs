e3937ce2b9f19b49c381ea680052cb0e8eb341aa
julietgobran00 <71473941+julietgobran00@users.noreply.github.com>
2022-10-11 13:27:47 +1100

    Resolution functionality

--- Major Project/Assets/Scripts/UI/SettingsMenu.cs
+++ Major Project/Assets/Scripts/UI/SettingsMenu.cs
@@ -6,11 +6,15 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 using System.Linq;
+using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
     // Sets volume as slider moves
     public AudioMixer aM;
+    public List<ResolutionItem> resolutions = new List<ResolutionItem>();
+    private int selectedResolution;
+    public TMP_Text resolutionLabel;
 
     void Start()
     {
@@ -23,4 +27,41 @@ public class SettingsMenu : MonoBehaviour
     {
         aM.SetFloat("MasterVolume", vol);
     }
+
+    public void ResolutionLeft()
+    {
+        selectedResolution--;
+        if (selectedResolution < 0)
+        {
+            selectedResolution = 0;
+        }
+        UpdateResLabel();
+    }
+
+    public void ResolutionRight()
+    {
+        selectedResolution++;
+        if (selectedResolution > resolutions.Count - 1)
+        {
+            selectedResolution = resolutions.Count - 1;
+        }
+        UpdateResLabel();
+    }
+
+    public void UpdateResLabel()
+    {
+        resolutionLabel.text = resolutions[selectedResolution].horizontal.ToString() + " x " + resolutions[selectedResolution].vertical.ToString();
+    }
+
+    public void UpdateOnOk()
+    {
+        Screen.SetResolution(resolutions[selectedResolution].horizontal, resolutions[selectedResolution].vertical, false);
+    }
+}
+
+[System.Serializable]
+public class ResolutionItem
+{
+    public int horizontal;
+    public int vertical;
 }
