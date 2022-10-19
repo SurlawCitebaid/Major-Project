8527714e1859fd08b137bd30857c5fe89301b80c
Diminious <matthewzengoski@gmail.com>
2022-10-20 00:23:05 +1100

    consumables now triggers, magnetise to player

--- Major Project/Assets/Scripts/Items/ConsumedItem.cs
+++ Major Project/Assets/Scripts/Items/ConsumedItem.cs
@@ -5,26 +5,49 @@ using UnityEngine;
 public class ConsumedItem : MonoBehaviour
 {
     [SerializeField] private int id;
-    private void OnCollisionEnter2D(Collision2D other) {
+    bool inside = false;
+    float force = 6f;
+    float timeStamp;
+    Transform magnet;
+
+    private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.CompareTag("Player"))
         {
-            switch (id){
+            
+            PickUpItem(id);
+            Destroy(gameObject);
+        } else if (other.gameObject.CompareTag("Magnet")){
+            timeStamp = Time.time;
+            magnet = other.gameObject.transform;
+            inside = true;
+            GetComponent<Rigidbody2D>().gravityScale = 0;
+        }
+    }
+
+    private void Update() {
+        if (inside) {
+            Vector2 magnetDir = (magnet.position - transform.position);
+            GetComponent<Rigidbody2D>().velocity = magnetDir.normalized * force * (Time.time + timeStamp)/timeStamp;
+        }
+    }
+
+    void PickUpItem(int id){
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        switch (id){
                 case 0:
                     int value = Random.Range(1, 10);
-                    other.gameObject.GetComponent<PlayerController>().AddCurrency(value);//add currency to player
+                    player.GetComponent<PlayerController>().AddCurrency(value);//add currency to player
                     FindObjectOfType<AudioManager>().Play("CoinCollect");
                     //play collect particles
                     break;
                 case 1:
-                    other.gameObject.GetComponent<PlayerController>().RegenHealth(); //heal
+                    player.GetComponent<PlayerController>().RegenHealth(); //heal
                     FindObjectOfType<AudioManager>().Play("Heal");
                     //play collect particles
                     break;
                 default:
                 break;
             }
-            
-            Destroy(gameObject);
-        }
     }
 }
